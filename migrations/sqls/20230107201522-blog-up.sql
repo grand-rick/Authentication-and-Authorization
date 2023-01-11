6dd7234ce1e5005d5267f3246aa2ee6e2273012b
Patrick Kabuga <kabugap02@gmail.com>
2023-01-11 05:16:26 +0300

    Updated the migrations

--- migrations/sqls/20230107201522-blog-up.sql
+++ migrations/sqls/20230107201522-blog-up.sql
@@ -1,2 +1,2 @@
 CREATE TABLE articles (id SERIAL PRIMARY KEY, title VARCHAR(50), content text);
-CREATE TABLE users (id SERIAL PRIMARY KEY, first_name VARCHAR(50), last_name VARCHAR(50), username VARCHAR(50), password_digest VARCHAR(50));
+CREATE TABLE users (id SERIAL PRIMARY KEY, username VARCHAR(50), password_digest VARCHAR);
