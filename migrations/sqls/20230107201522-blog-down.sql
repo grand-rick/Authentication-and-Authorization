c621b9e1a596a145b8e2429f19c5d9abe3600868
Patrick Kabuga <kabugap02@gmail.com>
2023-01-12 04:38:49 +0300

    Added a users-tble migration

--- migrations/sqls/20230107201522-blog-down.sql
+++ migrations/sqls/20230107201522-blog-down.sql
@@ -1,2 +1,1 @@
 DROP TABLE articles;
-DROP TABLE users;
--- migrations/sqls/20230107201522-blog-up.sql
+++ migrations/sqls/20230107201522-blog-up.sql
@@ -1,2 +1,1 @@
 CREATE TABLE articles (id SERIAL PRIMARY KEY, title VARCHAR(50), content text);
-CREATE TABLE users (id SERIAL PRIMARY KEY, username VARCHAR(50), password_digest VARCHAR);
